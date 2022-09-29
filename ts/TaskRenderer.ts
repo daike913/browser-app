e13deefd839dfd651e30a270c6caeeb672b80f49
daike913 <daikichi.kyoshin@gmail.com>
2022-09-30 08:04:38 +0900

    p.190 taskを追加できるようにする

---
+++ ts/TaskRenderer.ts
@@ -0,0 +1,32 @@
+import { Task } from "./Task";
+
+export class TaskRenderer {
+  constructor(private readonly todoList: HTMLElement) {}
+
+  append(task: Task) {
+    const taskEL = this.render(task)
+
+    this.todoList.append(taskEL)
+  }
+
+  private render(task: Task) {
+    // <div class="taskItem">
+    //   <span>タイトル</span>
+    //   <button>削除</button>
+    // </div>
+
+    const taskEl = document.createElement('div')
+    const spanEl = document.createElement('span')
+    const deleteButtonEL = document.createElement('button')
+
+    taskEl.id = task.id
+    taskEl.classList.add('task-item')
+
+    spanEl.textContent = task.title
+    deleteButtonEL.textContent = '削除'
+
+    taskEl.append(spanEl, deleteButtonEL)
+
+    return taskEl
+  }
+}
