ff100cc2fe7df85c094c5bc50fb4ce5b8fd6fd67
daike913 <daikichi.kyoshin@gmail.com>
2022-10-12 09:19:33 +0900

    p.217 localStorageのバリデーション実装

--- ts/TaskCollection.ts
+++ ts/TaskCollection.ts
@@ -1,14 +1,24 @@
-import { Status, Task } from './Task'
+import { Status, Task, TaskObject } from './Task'
+
+const STORAGE_KEY = 'TASKS'
 
 export class TaskCollection {
-  private tasks: Task[] = []
+  private readonly storage
+  private tasks
+
+  constructor() {
+    this.storage = localStorage
+    this.tasks = this.getStoredTasks()
+  }
 
   add(task: Task) {
     this.tasks.push(task)
+    this.updateStorage()
   }
 
   delete(task: Task) {
     this.tasks = this.tasks.filter(({ id }) => id !== task.id)
+    this.updateStorage()
   }
 
   find(id: string) {
@@ -24,5 +34,33 @@ export class TaskCollection {
 
   filter(filterStatus: Status) {
     return this.tasks.filter(({ status }) => status === filterStatus)
+  }
+
+  private getStoredTasks() {
+    const jsonString = this.storage.getItem(STORAGE_KEY)
+    if(!jsonString) return []
+
+    try {
+      const storedTask = JSON.parse(jsonString)
+
+      assertIsTaskObjects(storedTask)
+
+      const tasks = storedTask.map((task) => new Task(task))
+
+      return tasks
+    } catch {
+      this.storage.removeItem(STORAGE_KEY)
+      return []
+    }
+  }
+
+  private updateStorage() {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks))
+  }
+}
+
+function assertIsTaskObjects(value: any): asserts value is TaskObject[] {
+  if (!Array.isArray(value) || !value.every((item) => Task.validate(item))) {
+    throw new Error('引数「value」は TaskObject[] 型と一致しません。')
   }
 }
